16358532c165ba2669dbd7abc20b6a4b7a55c6b7
saji <skm.kiani@gmail.com>
2024-09-14 16:32:59 +0330

    pwa

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,55 @@ export default defineConfig({
     host: "::",
     port: "8080",
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {  
+        description: 'crypto tracker app developed by saji',
+        name: 'Crypto Tracker',  
+        short_name: 'Crypto Tracker',  
+        start_url: '/',  
+        display: 'standalone',  
+        background_color: '#ffffff',  
+        theme_color: '#ffffff',  
+        screenshots: [
+          {
+            src: 'screenshot.png',
+            sizes: '320x320',
+            label: 'crypto tracker',
+            form_factor: 'narrow'
+          },
+          {
+            src: 'screenshot.png',
+            sizes: '320x320',
+            label: 'crypto tracker',
+            form_factor: 'wide'
+          },
+        ],
+        icons: [  
+          {  
+            src: 'icon-72.png',  
+            sizes: '72x72',  
+            type: 'image/png',  
+          },  
+          {  
+            src: 'icon-96.png',  
+            sizes: '96x96',  
+            type: 'image/png',  
+          },  
+          {  
+            src: 'icon-144.png',  
+            sizes: '200x200',  
+            type: 'image/png',  
+          },  
+        ],  
+      },
+    })
+  ],
   resolve: {
     alias: [
       {
